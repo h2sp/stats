f7b130a5ec5b84b261771ff36746d83fc70bfaf5
h2sp <htwosp+github@gmail.com>
2014-02-12 07:35:24 +0900

    histgram by numpy

--- sample_code/histogram.py
+++ sample_code/histogram.py
@@ -1,3 +1,26 @@
+import csv
 import numpy as np
 
-np.histogram(
+#csvfile = open('./sample_data/1-1.csv', 'r')
+with open('./sample_data/1-1.csv', newline='') as f:
+    sincyou = csv.reader(f, dialect='unix', delimiter=',')
+    for row in sincyou:
+        #print(','.join(row))
+        print(row)
+
+
+#    hist = {}
+#    i = 0
+#    for x in data:
+#        i = i + 1
+#        print(i)
+#        print(x)
+#        hist[x] = hist.get(x, 0) + 1
+#print(hist)
+
+data = np.loadtxt('./sample_data/1-1.csv', delimiter=',')
+print(data)
+hist, binedge = np.histogram(data)
+print(hist, binedge)
+
+#help(np.histogram)
